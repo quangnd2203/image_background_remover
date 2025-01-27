9eae30b6c6230173d23cb1283b0c84e80c5f6ad7
Netesh5 <netesh.paudel@gmail.com>
2025-01-27 20:53:51 +0545

    feat: implement background removal functionality in BackgroundRemoverService

--- lib/src/background_remover_service.dart
+++ lib/src/background_remover_service.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
+import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:onnxruntime/onnxruntime.dart';
 
@@ -30,7 +32,6 @@ class BackgroundRemoverService {
       final rawAssetFile = await rootBundle.load(assetFileName);
       final bytes = rawAssetFile.buffer.asUint8List();
       _session = OrtSession.fromBuffer(bytes, sessionOptions);
-
       if (kDebugMode) {
         log('ONNX session created successfully.');
       }
@@ -38,6 +39,43 @@ class BackgroundRemoverService {
       if (kDebugMode) {
         log('Error creating ONNX session: $e');
       }
+    }
+  }
+
+  Future<ui.Image> removeBg(Uint8List imageBytes) async {
+    if (_session == null) {
+      throw Exception("ONNX session not initialized");
+    }
+
+    /// Decode image and resize
+    final originalImage = await decodeImageFromList(imageBytes);
+    log('Original image size: ${originalImage.width}x${originalImage.height}');
+    final resizedImage = await _resizeImage(originalImage, 320, 320);
+
+    /// Convert image to tensor
+    final rgbFloats = await _imageToFloatTensor(resizedImage);
+    final inputTensor = OrtValueTensor.createTensorWithDataList(
+      Float32List.fromList(rgbFloats),
+      [1, 3, 320, 320],
+    );
+
+    /// Prepare inputs and run inference
+    final inputs = {'input.1': inputTensor};
+    final runOptions = OrtRunOptions();
+    final outputs = await _session!.runAsync(runOptions, inputs);
+    inputTensor.release();
+    runOptions.release();
+
+    /// Convert output tensor to an image
+    final outputTensor = outputs?[0]?.value;
+    if (outputTensor is List) {
+      final mask = outputTensor[0][0];
+
+      final resizedMask =
+          resizeMask(mask, originalImage.width, originalImage.height);
+      return _applyMaskToOriginalSizeImage(originalImage, resizedMask);
+    } else {
+      throw Exception('Unexpected output format from ONNX model.');
     }
   }
 }
