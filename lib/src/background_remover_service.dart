2a8bd4b914fbebcd39da69e512c9324915447f5b
Netesh5 <netesh.paudel@gmail.com>
2025-01-27 20:52:04 +0545

    feat: enhance background remover service with ONNX session creation and debug logging

--- lib/src/background_remover_service.dart
+++ lib/src/background_remover_service.dart
@@ -1,5 +1,7 @@
 import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:onnxruntime/onnxruntime.dart';
 
 class BackgroundRemoverService {
@@ -15,11 +17,27 @@ class BackgroundRemoverService {
   Future<void> initializeOrt() async {
     try {
       OrtEnv.instance.init();
-
       await _createSession();
-      log('ORT Session created', name: "BackgroundRemoverService");
     } catch (e) {
       log(e.toString());
+    }
+  }
+
+  Future<void> _createSession() async {
+    try {
+      final sessionOptions = OrtSessionOptions();
+      const assetFileName = 'assets/u2netp.onnx';
+      final rawAssetFile = await rootBundle.load(assetFileName);
+      final bytes = rawAssetFile.buffer.asUint8List();
+      _session = OrtSession.fromBuffer(bytes, sessionOptions);
+
+      if (kDebugMode) {
+        log('ONNX session created successfully.');
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        log('Error creating ONNX session: $e');
+      }
     }
   }
 }
