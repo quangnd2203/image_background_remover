6783bbe23a7dcf2feec3431693e2ca0ab8bff152
Netesh5 <netesh.paudel@gmail.com>
2025-01-28 10:04:38 +0545

    chore: update Podfiles and xcconfig files for macOS and iOS, add image_picker dependency, and register file selector plugins

---
+++ example/lib/image_picker.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:developer';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerService {
+  static ImagePicker imagePicker = ImagePicker();
+
+  static ValueNotifier<File?> pickedFile = ValueNotifier(null);
+
+  static Future pickImage(BuildContext context) async {
+    try {
+      final file = await imagePicker.pickImage(source: ImageSource.gallery);
+      if (file == null) {
+        return;
+      } else {
+        pickedFile.value = File(file.path);
+      }
+    } on Exception catch (e) {
+      log(e.toString());
+    }
+  }
+}
