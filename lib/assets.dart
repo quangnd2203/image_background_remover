08ac43f05c11088e75c2fb82d2a0d652f8cec22a
Netesh5 <netesh.paudel@gmail.com>
2025-01-28 18:27:18 +0545

    fix: update modelPath in Assets class to use package-relative path

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,4 +1,4 @@
 class Assets {
   static const modelPath =
-      '/Users/neteshpaudel/Projects/flutter_background_remover/assets/model.onnx';
+      'packages/image_background_remover/assets/model.onnx';
 }
